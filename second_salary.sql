fb717d9c45e2ee980d52591e54126d3caf7ca3e4
Marchukova01 <90204625+Marchukova01@users.noreply.github.com>
2024-03-29 16:35:49 +0300

    Update second_salary.sql

--- second_salary.sql
+++ second_salary.sql
@@ -1,1 +1,15 @@
+SELECT r.person_id, 
+       r.department_id,
+       r.salary
+FROM
+(SELECT e.id AS person_id, 
+        d.id  AS department_id, 
+        e.salary AS salary, 
+        DENSE_RANK() OVER(PARTITION by d.name ORDER BY salary DESC) 
+        AS place
 
+    FROM Employee e 
+    JOIN Department d
+    ON e.departmentId = d.id) 
+    AS r
+WHERE r.place = 2
