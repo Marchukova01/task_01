7111145428998f0a9c6a0e98b125c958c21492b7
Marchukova01 <90204625+Marchukova01@users.noreply.github.com>
2024-03-29 16:35:23 +0300

    Update brackets.py

--- brackets.py
+++ brackets.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def brackets_seq(s):
+    stack = []
+    pairs = {'}':'{', ']':'[', ')':'('}
+    flag = 1
+        
+    for c in s:
+        if c in '{[(':
+            stack.append(c)
+        elif c in '}])':
+            if (len(stack) == 0):
+                flag = 0
+                break
+            else:
+                result = stack.pop() 
+                if (result != pairs[c]):
+                    flag = 0
+                    break
+                    
+    if ((len(stack) == 0) and (flag == 1)):
+        print("YES")
+    else:
+        print("NO")
 
